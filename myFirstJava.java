4cb1f8217470781d273892de083f5a4885965427
leslie wilson <lesliewilson@mcdhcp3-182.marlboro.edu>
2018-03-08 17:59:24 -0500

    first

---
+++ myFirstJava.java
@@ -0,0 +1,13 @@
+public class myFirstJava {
+
+
+public static void main(String []args)
+{
+    System.out.println("Hello World");
+    int number1 = 5;
+    int number2 = 7;
+    int result = number1 + number2;
+
+    System.out.println(result);
+    }
+}
